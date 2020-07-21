567b51e2ce1de7bf57539a34dca87f329ef4545f
bonghoa <hoa.nguyen@sentifi.com>
2020-07-21 17:44:46 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ReadabilityLola-bonghoa15", # Replace with your own username
+    name="ReadabilityLola", # Replace with your own username
     version="0.0.1",
     author="bonghoa",
     author_email="nguyendieuhoa15@gmail.com",
