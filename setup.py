27468c231b930bb9c053346f2b1555b82a5b411b
bonghoa <hoa.nguyen@sentifi.com>
2020-07-21 16:53:13 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 from distutils.core import setup
 setup(
-  name = 'readability_lola',         # How you named your package folder (MyLib)
-  packages = ['readability_lola'],   # Chose the same as "name"
+  name = 'ReadabilityLola',         # How you named your package folder (MyLib)
+  packages = ['ReadabilityLola'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'readability score of local languages',   # Give a short description about your library
   author = 'bonghoa',                   # Type in your name
   author_email = 'nguyendieuhoa15@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/bonghoa15',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/bonghoa15/readability_lola/archive/v_011.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/bonghoa15/ReadabilityLola/archive/v_011.tar.gz',    # I explain this later on
   keywords = ['READABILITY', 'LANGUAGES', 'HINDI','BAHASA'],   # Keywords that define your package best
   install_requires=[
           'numpy',
